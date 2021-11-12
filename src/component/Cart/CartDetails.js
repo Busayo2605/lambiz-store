4fdb3108ca44caed5960cde5c35a0d21df203586
Busayo <laminabusayo@gmail.com>
2021-11-12 09:02:27 +0100

    Worked on Pay with card button

--- src/component/Cart/CartDetails.js
+++ src/component/Cart/CartDetails.js
@@ -9,19 +9,28 @@ const CartDetails = ({ item }) => {
   const history = useHistory();
   const onToken = (token) => {
     try {
+      fetch("/save-stripe-token", {
+        method: "POST",
+        body: JSON.stringify(token),
+      }).then((response) => {
+        response.json().then((data) => {
+          console.log(`We are in business, ${data.email}`);
+        });
+      });
       dispatch({
         type: "ADD_TO_ORDER",
         payload: item,
       });
       setStatus("SUCCESS");
+      history.push("/store");
     } catch (error) {
       dispatch({
         type: "ADD_TO_ORDER",
         payload: item,
       });
       setStatus("FAILED");
+      history.push("/store");
     }
-    history.push("/store");
   };
 
   return (
@@ -31,10 +40,13 @@ const CartDetails = ({ item }) => {
         <StripeCheckout
           name="Lambiz Store"
           token={onToken}
-          currency='usd'
+          currency="USD"
           amount={Math.round(Total) * 100}
           stripeKey="pk_test_51HPNYdLVxHXfNXqKFvaPbSVxUR2RRqM7SiQvtOV473cKvhEn8Z43hz4bLmj8iegOMls6HPkzDjWGtdaBcN1EB4Q800BDwWVR5z"
-        />
+          allowRememberMe
+        >
+          <button className="btn btn-primary">PAY WITH CARD</button>
+        </StripeCheckout>
         <RemoveBtn
           onClick={() =>
             dispatch({
